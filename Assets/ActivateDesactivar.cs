2cb74f6c59d4617c458909396ce59f3116522cc4
FrancoMascardi <101577478+FrancoMascardi@users.noreply.github.com>
2022-05-19 09:00:29 -0300

    Finished Activate/Desactivate

---
+++ Assets/ActivateDesactivar.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ActivateDesactivar : MonoBehaviour
+{
+    public GameObject InputField;
+    public bool wasPressed;
+    void Start()
+    {
+        //wasPressed = false;
+    }
+    public void ActivateAndDeactivate()
+    {
+        //if (!wasPressed)
+        //{
+        //    InputField.SetActive(false);
+        //    wasPressed = true;
+        //}
+        //else
+        //{
+        //    InputField.SetActive(true);
+        //    wasPressed = false;
+        //}
+
+
+        //if (InputField.activeInHierarchy)
+        //{
+        //    InputField.SetActive(false);
+        //}
+        //else
+        //{
+        //    InputField.SetActive(true);
+        //}
+
+
+        InputField.SetActive(!InputField.activeInHierarchy);
+        //si es verdadero, el ! me lo devuelve como falso y si esta en falso me lo devuelve en verdadero
+
+    }
+}
